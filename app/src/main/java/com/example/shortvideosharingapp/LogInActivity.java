2e563f9983b28880bf188251032c8a084d85f483
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-08-07 01:37:59 +0530

    created the object of the reistration activity in the LogActivity.java so, that when we press the button it can switch.

--- app/src/main/java/com/example/shortvideosharingapp/LogInActivity.java
+++ app/src/main/java/com/example/shortvideosharingapp/LogInActivity.java
@@ -10,5 +10,13 @@ public class LogInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        //creating the object of the textViewButton-signUpTextView
+        TextView button = findViewById(R.id.signUpTextView);
+        button.setOnClickListner(new.OnClickListner(){
+
+            public void onClick(View v) {
+                startActivity(new Intent(LogInActivity.this,RegisterActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/shortvideosharingapp/RegisterActivity.java
+++ app/src/main/java/com/example/shortvideosharingapp/RegisterActivity.java
@@ -10,5 +10,15 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+
+        //object of the button, which needs data
+        TextView button = findViewById(R.id.alreadyHaveAccount);
+        //settting up the set on click listner
+        button.setOnClickListner(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        })
     }
 }
